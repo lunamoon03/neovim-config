80cea14f2fe00fb230ec81fa0b4e500c354f2005
Luna <luna.wilkes@gmail.com>
2024-10-04 03:29:21 +1300

    Add Neotree
    TODO: Unresolved conflict with numbers.vim

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -10,7 +10,20 @@ return {
 	},
 	{ "navarasu/onedark.nvim" },
 	{ "mfussenegger/nvim-jdtls" },
-	{ "myusuf3/numbers.vim" },
+	{ 
+		"myusuf3/numbers.vim",
+		numbers_exclude = {"Neotree", "filesystem","neo-tree", "Neotree.neo-tree"}
+	},
 	{ "farmergreg/vim-lastplace" },
-	{ "mfussenegger/nvim-dap" }
+	{ "mfussenegger/nvim-dap" },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		}
+	}
 }
