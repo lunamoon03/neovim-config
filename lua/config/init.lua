af9848e61770108e9372ca45d95da1cd035204cb
Luna <neovx.23@proton.me>
2024-10-07 03:57:46 +1300

    Merge defaults.lua into options.lua Why were they different?? Who knows..

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,5 +1,4 @@
-require "config.lazy"
-require "config.plugins"
-require "config.options"
 require "config.autocmds"
-require "config.defaults"
+require "config.lazy"
+require "config.options"
+require "config.plugins"
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,15 @@
+-- Indentation settings
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
-
-
+-- Telescope keybinds
+vim.keymap.set({"n","i"}, "<C-o>", "<esc>:Telescope fd<CR>", { noremap = true, silent = true})
+vim.keymap.set({"n","i"}, "<C-f>", "<esc>:Telescope live_grep<CR>", { noremap = true, silent = true})
+-- Make mousescrolling work properly with fugitive-vim blame
+vim.o.mouse="a"
+-- Split settings
+vim.o.splitright = true
+vim.o.splitbelow = true
+-- Pretty cursor line :3
+vim.o.cursorline = true
+vim.o.cursorlineopt = "both"
