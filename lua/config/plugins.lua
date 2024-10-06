00e8d80d6c62604557ae0367b84ead33665a396f
Luna <neovx.23@proton.me>
2024-10-07 04:48:07 +1300

    Add which-key.nvim

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -21,6 +21,19 @@ return {
 	{ "navarasu/onedark.nvim" },
 	-- file system stuff
 	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim",
+		},
+		config = function()
+			require "config.settings.neotree"
+		end,
+	},
+	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
 		dependencies = { 
@@ -36,22 +49,23 @@ return {
 		"nvim-telescope/telescope-ui-select.nvim",
 		dependencies = { "nvim-telescope/telescope.nvim" }
 	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-			-- "3rd/image.nvim",
-		},
-		config = function()
-			require "config.settings.neotree"
-		end,
-	},
-	-- git
+	-- git wrapper
 	{ "tpope/vim-fugitive", cmd = "Git" },
 	-- UI/qol
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({global = false})
+				end,
+				desc = "Buffer local keymaps (which-key)",
+			},
+		},
+	},
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
