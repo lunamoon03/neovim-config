82b90def594574fb78693f4225b3f2f1fe8b1c8b
Luna <neovx.23@proton.me>
2024-10-07 03:21:15 +1300

    Remove windows.vim Plugin conflicted with vim-fugitive and was also just kind of annoying

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -48,19 +48,6 @@ return {
 	-- git
 	{ "tpope/vim-fugitive", cmd = "Git" },
 	-- UI/qol
-	{
-		"anuvyklack/windows.nvim",
-		dependencies = {
-			"anuvyklack/middleclass",
-			"anuvyklack/animation.nvim"
-		},
-		config = function()
-			vim.o.winwidth = 10
-			vim.o.winminwidth = 10
-			vim.o.equalalways = false
-			require('windows').setup()
-		end,
-	},
 	{ 
 		"myusuf3/numbers.vim",
 		config = function()
