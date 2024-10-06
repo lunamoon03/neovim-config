ea21d1775b04a560bdb07ea849f6139cfed771be
Luna <neovx.23@proton.me>
2024-10-07 03:55:06 +1300

    Add more options

--- lua/config/defaults.lua
+++ lua/config/defaults.lua
@@ -1,3 +1,11 @@
+-- Telescope keybinds
 vim.keymap.set({"n","i"}, "<C-o>", "<esc>:Telescope fd<CR>", { noremap = true, silent = true})
-
 vim.keymap.set({"n","i"}, "<C-f>", "<esc>:Telescope live_grep<CR>", { noremap = true, silent = true})
+-- Make mousescrolling work properly with fugitive-vim blame
+vim.o.mouse="a"
+-- Split settings
+vim.o.splitright = true
+vim.o.splitbelow = true
+-- Pretty cursor line :3
+vim.o.cursorline = true
+vim.o.cursorlineopt = "both"
