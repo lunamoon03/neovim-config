af65dc1cfea9907391a8b3c97e83e727e0256b2c
Luna <neovx.23@proton.me>
2024-10-19 16:46:13 +1300

    Use leader in options.lua
    Also change Neotree and Bdelete cmds to be automatic

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,14 +3,16 @@ vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 -- File system keybinds
+vim.g.mapleader = " ";
 vim.keymap.set({"n","i"}, "<C-o>", "<esc>:Telescope fd<CR>", { noremap = true, silent = true})
 vim.keymap.set({"n","i"}, "<C-f>", "<esc>:Telescope live_grep<CR>", { noremap = true, silent = true})
-vim.keymap.set("n", "<space>tf", "<cmd>Telescope file_browser<CR>", { noremap = true })
-vim.keymap.set("n", "<space>of", "<cmd>Oil --float<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>tf", "<cmd>Telescope file_browser<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>o", "<cmd>Oil --float<CR>", { noremap = true, silent = true })
 -- Buffer closing keybinds
-vim.keymap.set("n", "q", ":Bdelete", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>q", "<cmd>Bdelete<CR>", { noremap = true, silent = true })
 -- Neotree keybind
-vim.keymap.set("n", "nt", ":Neotree", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>nt", "<cmd>Neotree<CR>", { noremap = true, silent = true })
+
 -- Make mousescrolling work properly with fugitive-vim blame
 vim.o.mouse="a"
 -- Split settings
