6c2b92df3fdc7c693081c1a4674b630b09763aeb
Luna <neovx.23@proton.me>
2024-10-20 16:33:52 +1300

    WIP: add nvim-cmp

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -11,6 +11,20 @@ return {
 			require "config.settings.treesitter"
 		end,
 	},
+	  {
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+		  "hrsh7th/cmp-nvim-lsp",
+		  "hrsh7th/cmp-buffer",
+		  "hrsh7th/cmp-path",
+		  "hrsh7th/cmp-cmdline",
+		  "onsails/lspkind.nvim",
+		},
+		config = function()
+		  require "config.settings.cmp"
+		end,
+		event = "InsertEnter",
+	},
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
