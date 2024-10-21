146441116d869e187d3763e5465108079b6910b2
Luna <neovx.23@proton.me>
2024-10-21 22:15:37 +1300

    Set up JavaSE-19 and JavaSE-22 in jdtls

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,21 @@
 local config = {
-	cmd = { '/opt/homebrew/bin/jdtls' },
+	cmd = { "jdtls" },
 	root_dir = vim.fs.dirname(vim.fs.find({'gradle', '.git', 'mvnw'}, { upward = true })[1]),
+	settings = {
+		java = {
+			configuration = {
+				runtimes = {
+					{
+						name = "JavaSE-19",
+						path = "/Users/lunawilkes/Library/Java/JavaVirtualMachines/openjdk-19.0.2/Contents/Home",
+					},
+					{
+						name = "JavaSE-22",
+						path = "/Users/lunawilkes/Library/Java/JavaVirtualMachines/jdk-22.0.2.jdk/Contents/Home/",
+					},
+				}
+			}
+		}
+	}
 }
 require('jdtls').start_or_attach(config)
