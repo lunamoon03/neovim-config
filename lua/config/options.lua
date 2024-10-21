59ad876564f4a4495a6d78d2728ac4223cd5ebaf
Luna <neovx.23@proton.me>
2024-10-21 16:52:50 +1300

    Add function to toggle lsp_lines
    Also make it off by default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,15 +8,27 @@ vim.g.mapleader = " ";
 
 -- File system keybinds
 vim.keymap.set({"n","i"}, "<C-o>", "<esc>:Telescope fd<CR>", { noremap = true, silent = true})
-vim.keymap.set({"n","i"}, "<C-f>", "<esc>:Telescope live_grep<CR>", { noremap = true, silent = true})
-vim.keymap.set("n", "<leader>tf", "<cmd>Telescope file_browser<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>o", "<cmd>Oil --float<CR>", { noremap = true, silent = true })
+vim.keymap.set({"n","i"}, "<C-f>", "<esc>:Telescope current_buffer_fuzzy_find<CR>", { noremap = true, silent = true})
+vim.keymap.set("n", "<leader>tf", "<cmd>Telescope file_browser<CR>", { noremap = true, silent = true, desc = "Telescope browser" })
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope live_grep<CR>", { noremap = true, silent = true, desc = "Grep files" })
+vim.keymap.set("n", "<leader>o", "<cmd>Oil --float<CR>", { noremap = true, silent = true, desc = "Floating Oil" })
 -- Buffer closing keybinds
 vim.keymap.set("n", "<leader>q", "<cmd>Bdelete<CR>", { noremap = true, silent = true })
 -- Neotree keybind
 vim.keymap.set("n", "<leader>nt", "<cmd>Neotree<CR>", { noremap = true, silent = true })
 -- floating window
 vim.keymap.set("n", "<leader>fl", "<cmd>Oil --float<CR><cmd>Bdelete<CR>", { noremap = true, silent = true })
+-- lsp_lines toggle
+function ToggleLspLines()
+	require("lsp_lines").toggle()
+	vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text})
+end
+vim.keymap.set("n", "<leader>l", ":lua ToggleLspLines()<CR>", { noremap = true, silent = true, desc = "Toggle lsp_lines" })
+-- hide virtual text re lsp_lines
+vim.diagnostic.config({
+  virtual_text = true,
+  virtual_lines = false,
+})
 -- Make mousescrolling work properly with fugitive-vim blame
 vim.o.mouse="a"
 -- Split settings
@@ -27,7 +39,3 @@ vim.o.cursorline = true
 vim.o.cursorlineopt = "both"
 -- Don't need to show mode with lualine :)
 vim.o.showmode = false
--- hide virtual text re lsp_lines
-vim.diagnostic.config({
-  virtual_text = false,
-})
--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -16,6 +16,7 @@ return {
 	},
 	{
 		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		lazy = true,
 		config = function()
 			require("lsp_lines").setup()
 		end,
