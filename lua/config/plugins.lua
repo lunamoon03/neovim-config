88f831d70af97b6ceaa284d230e4016dbbdd3c98
Luna <neovx.23@proton.me>
2024-10-26 21:40:47 +1300

    Add custom colours to onedark

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -9,7 +9,13 @@ return {
 				style = 'darker',
 				term_colors = true,
 				colors = {
-
+					light_red = "#d17277",
+					light_orange = "#e9c28a",
+					light_yellow = "#dfc184",
+					light_green = "#a0c180",
+					light_blue = "#73ade9",
+					light_purple = "#bb7bd7",
+					light_cyan = "6fb3bf",
 				},
 				highlights = {
 					["@lsp.type.modifier.java"] = { fg = "$purple" },
