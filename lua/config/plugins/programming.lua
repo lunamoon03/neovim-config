7a5d187c7b1f1dfd78144a1f8cff1e7a6a1bc295
Luna <neovx.23@proton.me>
2024-11-01 12:09:33 +1300

    Add coq dot completion workaround

--- lua/config/plugins/programming.lua
+++ lua/config/plugins/programming.lua
@@ -20,6 +20,9 @@ return {
         auto_start = 'shut-up',
         keymap = { recommended = false },
       }
+      -- dot not working workaround
+      -- ref https://github.com/ms-jpq/coq_nvim/issues/464#issuecomment-1250233282
+      vim.api.nvim_set_keymap('i', '.', '<cmd>lua vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(".<C-x><C-u><C-e>", true, false, true), "n", true)<CR>', { noremap = true })
       vim.api.nvim_set_keymap('i', '<Esc>', [[pumvisible() ? "\<C-e><Esc>" : "\<Esc>"]], { expr = true, silent = true })
       vim.api.nvim_set_keymap('i', '<C-c>', [[pumvisible() ? "\<C-e><C-c>" : "\<C-c>"]], { expr = true, silent = true })
       vim.api.nvim_set_keymap('i', '<BS>', [[pumvisible() ? "\<C-e><BS>" : "\<BS>"]], { expr = true, silent = true })
