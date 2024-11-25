6b701b7ba6312b61a60b102acc1de87aa63f5dab
Luna <neovx.23@proton.me>
2024-11-25 18:54:06 +1300

    Add vim tmux navigator

--- lua/config/plugins/qol.lua
+++ lua/config/plugins/qol.lua
@@ -75,6 +75,24 @@ return {
       require("leap").add_default_mappings()
     end,
   },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = true,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
   -- Misc
   {
     "brenton-leighton/multiple-cursors.nvim",
